3070bd134061d87fbe748c147f46e437e87a9190
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-07-04 23:12:48 +0530

    feat: add validation

--- src/transcriptr/validation.py
+++ src/transcriptr/validation.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-
+from pathlib import Path
 
 @dataclass
 class ValidationResult:
@@ -13,19 +13,19 @@ class ValidationResult:
         return len(self.errors) == 0
 
 
-# class InputValidator:
-#     """Validate input files"""
+class InputValidator:
+    """Validate input files"""
 
 
-# @classmethod
-# def validate(cls, config: Path) -> ValidationResult:
-#     result = ValidationResult
-#     cls._validate_fastqs(config, result)
-#     cls._validate_fastq_format(config, result)
-#     cls._validate_paired_reads(config, result)
-#     cls._validate_hisat2_index(config, result)
-#     cls._validate_gtf(config, result)
-#     cls._validate_sample_name(config, result)
-#     cls._validate_tools(config, result)
+@classmethod
+def validate(cls, config: Path) -> ValidationResult:
+    result = ValidationResult
+    cls._validate_fastqs(config, result)
+    cls._validate_fastq_format(config, result)
+    cls._validate_paired_reads(config, result)
+    cls._validate_hisat2_index(config, result)
+    cls._validate_gtf(config, result)
+    cls._validate_sample_name(config, result)
+    cls._validate_tools(config, result)
 
-#     return result
+    return result
